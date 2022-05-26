02b746071efc563fe355382c9fa990390d4351e9
DustALLi <vernik20021106@gmail.com>
2022-05-26 18:45:17 +0500

    3 шаг.

--- SLOMANO.cpp
+++ SLOMANO.cpp
@@ -7,24 +7,28 @@ using namespace glm;
 
 GLuint VBO;
 
-static void RenderSceneCB()
-{
+static void RenderSceneCB(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	
-	glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDisableVertexAttribArray(0);
 	
 	glutSwapBuffers();
 }
 
-static void CreateVertexBuffer() 
-{
-	vec3 Vertices[1];
-	Vertices[0] = vec3(0.0f, 0.0f, 0.0f);
+static void InitializeGlutCallbacks(){
+	glutDisplayFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer(){
+	vec3 Vertices[3];
+	Vertices[0] = vec3(-1.0f, -1.0f, 0.0f);
+	Vertices[1] = vec3(1.0f, -1.0f, 0.0f);
+	Vertices[2] = vec3(0.0f, 1.0f, 0.0f);
 	
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -38,9 +42,10 @@ int main(int argc, char** argv)
 	
 	glutInitWindowSize(1024, 768);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Tutorial 02");
+	glutCreateWindow("Tutorial 03");
 
 	glutDisplayFunc(RenderSceneCB);
+	InitializeGlutCallbacks();
 
 	GLenum res = glewInit();
 	if (res != GLEW_OK) {
